29496ce6038df68d90d142070609087eca1718b4
lfg357 <2931067203@qq.com>
2026-08-12 12:45:30 +0800

    M2: reserve HUD safe area from dive title

--- scenes/dive/dive.gd
+++ scenes/dive/dive.gd
@@ -2,9 +2,7 @@ extends Control
 
 @onready var terrain_world = $TerrainWorld
 @onready var drill_rig = $DrillRig
-@onready var title: Label = $Title
 @onready var instruction: Label = $Instruction
 
 func _ready() -> void:
-	title.text = tr("dive.title")
 	instruction.text = tr("dive.sandbox.instruction")
